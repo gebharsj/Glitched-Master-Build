fee9453ef54e14e17a532cdc4c8665104217b35e
PeteyPablo7 <petergartzke@Peters-MacBook-Pro-2.local>
2016-09-27 18:52:35 -0500

    Menu, items, etc....
    added items, menu, scripts, fixed scripts, updated sprites and more

---
+++ Assets/Scripts/moveBox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBox : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+
+
+	// Use this for initialization
+	void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
