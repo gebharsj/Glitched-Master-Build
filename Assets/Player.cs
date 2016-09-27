7dd0fb7d73841e4296ce4f5af840e54d7cb329d4
PeteyPablo7 <petergartzke@Peters-MacBook-Pro-2.local>
2016-09-26 21:49:24 -0500

    Starting to put build together
    started to put it all together

---
+++ Assets/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rigidPlayer;
+    public float speed = 5f;
+
+    [SerializeField]
+    private float playerSpeed;
+
+    private bool playerFacingRight;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerFacingRight = true;
+
+        rigidPlayer = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        float vertical = Input.GetAxis("Vertical");
+
+        HandleMovement(horizontal);
+
+        HandleMovement1(vertical);
+
+        Flip(horizontal);
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        rigidPlayer.velocity = new Vector2(horizontal * playerSpeed, rigidPlayer.velocity.y);
+    }
+
+    private void HandleMovement1(float vertical)
+    {
+        rigidPlayer.velocity = new Vector2(rigidPlayer.velocity.x, vertical * playerSpeed);
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !playerFacingRight || horizontal < 0 && playerFacingRight)
+        {
+            playerFacingRight = !playerFacingRight;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= 1;
+
+            transform.localScale = theScale;
+        }
+    }
+}
---
+++ Assets/Scripts/FlashlightToggle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightToggle : MonoBehaviour {
+
+    public GameObject Flashlight;
+    //GameObject SuperSpotlight;
+    // Use this for initialization
+    void Start ()
+    {
+        Flashlight = GameObject.FindGameObjectWithTag("Flashlight");
+        //SuperSpotlight= 
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (GameObject.Find("Spotlight").GetComponent<Light>().enabled == false )
+            {
+                GameObject.Find("Spotlight").GetComponent<Light>().enabled = true;
+                GameObject.Find("Spotlight").GetComponent<CircleCollider2D>().enabled = true;
+            }
+            else
+            {
+                GameObject.Find("Spotlight").GetComponent<Light>().enabled = false;
+                GameObject.Find("Spotlight").GetComponent<CircleCollider2D>().enabled = false;
+            }
+        }
+	}
+}
